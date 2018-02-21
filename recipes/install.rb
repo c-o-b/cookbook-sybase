b0903fbf2e5b843ae5ff47bfe0d088b39af800a8
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-02-21 15:45:33 -0500

    push first version of install recipe

---
+++ recipes/install.rb
@@ -0,0 +1,43 @@
+# Create log directory
+directory node[:sybase][:log_dir] do
+  owner node[:sybase][:user]
+  group node[:sybase][:group]
+  mode '0744'
+  recursive true
+  action :create
+end
+
+
+# Create base dir
+directory node[:sybase][:rsp][:user_install_dir] do
+  owner node[:sybase][:user]
+  group node[:sybase][:group]
+  mode  '0755'
+  recursive true
+  action :create
+end
+
+
+# Create data directory
+directory "#{node[:sybase][:rsp][:user_install_dir]}/data" do
+  owner node[:sybase][:user]
+  group node[:sybase][:group]
+  mode  '0755'
+  recursive true
+  action :create
+end
+
+
+# Disable exec-sheild per Sybase install guide: http://infocenter.sybase.com/help/index.jsp?topic=/com.sybase.infocenter.dc01002.0321/doc/html/jfo1260972820954.html
+execute 'Disable exec-shield' do
+  command "echo kernel.exec-shield=0 >> /etc/sysctl.conf && sysctl -p"
+  not_if 'grep -v \'^#\' /etc/sysctl.conf | grep "\kernel.exec-shield=0\b"' # returns 0 if kernel exec-shield is disabled
+end
+
+
+# Install Sybase
+execute 'Install Sybase' do
+  cwd     node[:sybase][:stage_dir]
+  command node[:sybase][:install_cmd]
+  action  :run
+end
