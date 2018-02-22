033ecc51d48cb69589309442a3a729d3c8870731
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-02-22 16:49:22 -0500

    move packages to new file

---
+++ attributes/packages.rb
@@ -0,0 +1,38 @@
+default[:sybase][:pkgs] = %w[
+  glib2
+  openmotif
+  libXp
+  libXt
+  libXtst
+  libXi
+  libXmu
+  libXext
+  libSM
+  libICE
+  libX11
+  libXtst-devel
+  libXi-devel
+  openmotif-devel
+  libXmu-devel
+  libXt-devel
+  libXext-devel
+  libXp-devel
+  libX11-devel
+  libSM-devel
+  libICE-devel
+]
+
+
+default[:sybase][:pkgs_i686] = %w[
+  glib2.i686
+  openmotif.i686
+  libXp.i686
+  libXt.i686
+  libXtst.i686
+  libXi.i686
+  libXmu.i686
+  libXext.i686
+  libSM.i686
+  libICE.i686
+  libX11.i686
+]
