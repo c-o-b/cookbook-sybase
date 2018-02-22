ffd87501d5feac85c9b1da392b87c9f906add92a
Joseph Sirianni <joe.sirianni@bluemedora.com>
2018-02-22 16:50:27 -0500

    use sybase useR

--- recipes/stage_software.rb
+++ recipes/stage_software.rb
@@ -1,14 +1,14 @@
-# TODO, do not download install media if already installed
-# TODO, verify that chef cache actually cleans up after itself
-
 remote_file "#{node[:sybase][:stage_dir]}/#{node[:sybase][:source_archive]}" do
   source "file://#{node[:sybase][:source_archive_path]}"
+  owner  node[:sybase][:user]
+  group  node[:sybase][:group]
   action :create_if_missing
 end
 
 
+# Extract as root / sudo user but retain ownership (sybase user)
 execute "Extract archive" do
-  cwd      node[:sybase][:stage_dir]
+  cwd     node[:sybase][:stage_dir]
   command "tar xvf #{node[:sybase][:source_archive]}"
   not_if  { ::File.exist?("#{node[:sybase][:stage_dir]}/#{node[:sybase][:installer]}") }
 end
@@ -16,7 +16,7 @@ end
 
 template "#{node[:sybase][:stage_dir]}/#{node[:sybase][:rsp][:file]}" do
   source node[:sybase][:rsp][:source]
-  owner  "root"
-  group  "root"
+  owner  node[:sybase][:user]
+  group  node[:sybase][:group]
   mode   "0744"
 end
